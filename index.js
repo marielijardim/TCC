c6eeb0ad85276cda597debb307bc77acf9c7010a
Marieli <marielijardim190@gmail.com>
2022-06-20 17:39:24 -0300

    Arquivo Index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function(req,res){
+    res.send("Ola Mundo!")
+})
+
+app.listen(3000,function(){
+    console.log("Conexão inicializada na porta 3000")
+})
