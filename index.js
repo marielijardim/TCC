88c6f805a594dd10bd7f30d874b596b5b27ac884
Marieli <81644693+marielijardim@users.noreply.github.com>
2022-06-27 16:23:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get('/', function(req,res){
     res.send("Ola!")
 })
 
-app.get('/mensagem', function(req,res){
+app.get('/msg', function(req,res){
     res.send("Essa mensagem é automática!")
 })
 
