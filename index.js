d7c43d6fa66857542003a51ff6fb9be07c2b1cbe
Marieli <marielijardim190@gmail.com>
2022-06-27 16:44:51 -0300

    Adicionando a rota sobre

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ app.get('/msg', function(req,res){
     res.send("Essa mensagem é automática!")
 })
 
+app.get('/sobre', function(req,res){
+    res.send("Esta paginna esta sendo desenvolvida!")
+})
+
+
 app.listen(3000,function(){
     console.log("Conexão inicializada ")
 })
