c0bcfdae26bddd3e32624df51bfc02281c7e6418
Marieli <marielijardim190@gmail.com>
2022-06-29 17:11:43 -0300

    Arrumando algumas coisas

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express')
 const app = express()
+var bodyParser =  require("body-parser")
+var cookieParser = require("cookie-parser")
+var path = require("path")
+
+
+app.use(cookieParser())
+
+app.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.set("view engine","ejs")
+
+app.set(express.static(path.join(__dirname,"public")))
 
 app.get('/', function(req,res){
     res.send("Ola!")
-})
-
-app.get('/msg', function(req,res){
-    res.send("Essa mensagem é automática!")
-})
-
-app.get('/sobre', function(req,res){
-    res.send("Esta paginna esta sendo desenvolvida!")
 })
 
 
