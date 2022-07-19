90fa470b041d22899b4dcd42854e7602cdc747bd
Marieli <marielijardim190@gmail.com>
2022-07-19 17:42:00 -0300

    Adição da rota mensagem

---
+++ config/conexao.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose')
+const uri = "mongodb://mari:mari0206@ac-t9z9cxu-shard-00-00.gg9rckm.mongodb.net:27017,ac-t9z9cxu-shard-00-01.gg9rckm.mongodb.net:27017,ac-t9z9cxu-shard-00-02.gg9rckm.mongodb.net:27017/?ssl=true&replicaSet=atlas-xtcsbd-shard-0&authSource=admin&retryWrites=true&w=majority" //colocar o link para acesso ao mongodb
+
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedtopology: true })
+
+module.exports = mongoose 
--- index.js
+++ index.js
@@ -1,9 +1,53 @@
 const express = require('express')
 const app = express()
+var bodyParser =  require("body-parser")
+var cookieParser = require("cookie-parser")
+var path = require("path")
+var Usuario = require("./model/usuario")
+
+
+app.use(cookieParser())
+
+app.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.set("view engine","ejs")
+
+app.use(express.static(path.join(__dirname,"public")))
 
 app.get('/', function(req,res){
-    res.send("Ola!")
+    res.render('index.ejs', {})
 })
+
+
+
+app.get('/add',function(req,res){
+    res.render("adiciona.ejs")
+})
+
+
+
+app.post('/add',function(req,res){
+    var usuario = new Usuario({
+        nome: req.body.txtNome,
+        email: req.body.txtEmail,
+        senha: req.body.txtSenha,
+        foto: req.body.txtFoto
+    })
+    usuario.save(function(err){
+        if(err){
+            console.log(err)
+    
+        }else{
+            res.redirect('/');
+         
+        }
+    })
+    
+})
+
+
 
 app.listen(3000,function(){
     console.log("Conexão inicializada ")
